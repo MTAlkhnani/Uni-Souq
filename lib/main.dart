63c591c5b3717b3d5eab12e70f7f8d560c0e85f3
Mostafa Othman <65511802+mostafaSWE@users.noreply.github.com>
2024-02-03 16:47:13 +0300

    updating the gradle

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-choco upgrade dart-sdkimport 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:unisouq/models/firebase_options.dart';
