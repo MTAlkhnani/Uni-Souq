e31daa6e08305008490a6f355d17181a44c69efc
Mostafa Othman <65511802+mostafaSWE@users.noreply.github.com>
2024-02-03 15:51:45 +0300

    Changes to the generate questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+choco upgrade dart-sdkimport 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:unisouq/firebase_options.dart';
